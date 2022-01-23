5d2053058f2375a812c9e9bb18235cc226a7fea0
Elaine <elaine5030331@gmail.com>
2022-01-23 10:16:18 +0800

    btn - modal內容修改

--- champion/champion.js
+++ champion/champion.js
@@ -3,9 +3,7 @@ let champions = {};
 const heroCardTemplate = document.querySelector('#hero-card');
 const heroBox = document.querySelector('.hero-card-row');
 
-fetch(
-  'https://ddragon.leagueoflegends.com/cdn/10.22.1/data/zh_TW/champion.json'
-)
+fetch('https://ddragon.leagueoflegends.com/cdn/10.22.1/data/zh_TW/champion.json')
   .then(function (response) {
     return response.json();
   })
@@ -22,32 +20,39 @@ function showHeros() {
     heroCard.querySelector(
       '.card-img-top'
     ).src = `https://cdngarenanow-a.akamaihd.net/games/lol/2020/LOLwebsite/champion/${hero.id}_0.jpg`;
-    heroCard.querySelector('.card-title').innerText = `${index + 1}. ${
-      hero.id
-    } - ${hero.name}`;
-    heroCard.querySelector('.card-text').innerText =
-      hero.blurb.substring(0, 30) + '...';
+    heroCard.querySelector('.card-title').innerText = `${index + 1}. ${hero.id} - ${hero.name}`;
+    heroCard.querySelector('.card-text').innerText = hero.blurb.substring(0, 30) + '...';
 
-    heroCard.querySelector('.btn').onclick = function () {
-      document.querySelector(
-        '#exampleModalLabel'
-      ).innerText = `${hero.id} - ${hero.name}`;
+    heroCard.querySelector('.detail-btn').onclick = function () {
+      document.querySelector('#exampleModalLabel').innerText = `${hero.id} - ${hero.name}`;
       document.querySelector(
         '.modal-img'
       ).src = `https://cdngarenanow-a.akamaihd.net/games/lol/2020/LOLwebsite/champion/${hero.id}_0.jpg`;
-      Object.keys(hero.stats).forEach((detail) => {
-        switch (detail) {
-          case 'hp':
-          case 'movespeed':
-          case 'armor':
-          case 'spellblock':
-          case 'attackrange':
-            document.querySelector(
-              `.${detail}`
-            ).innerText = `${detail.toUpperCase()} : ${hero.stats[detail]}`;
-            break;
-        }
-      });
+
+      document.querySelector('.hp').innerText = `Hp : ${hero.stats.hp}`;
+      document.querySelector('.movespeed').innerText = `Move Speed : ${hero.stats.movespeed}`;
+      document.querySelector('.armor').innerText = `Armor : ${hero.stats.armor}`;
+      document.querySelector('.spellblock').innerText = `Spell Block : ${hero.stats.spellblock}`;
+      document.querySelector('.attackrange').innerText = `Attack Range : ${hero.stats.attackrange}`;
+
+      // Object.keys(hero.stats).forEach((detail) => {
+      //   switch (detail) {
+      //     case 'hp':
+      //     case 'movespeed':
+      //     case 'armor':
+      //     case 'spellblock':
+      //     case 'attackrange':
+      //       document.querySelector(`.${detail}`).innerText = `${detail.toUpperCase()} : ${
+      //         hero.stats[detail]
+      //       }`;
+      //       break;
+      //   }
+      // });
+    };
+
+    heroCard.querySelector('.vedio-btn').onclick = function () {
+      document.querySelector('#exampleModalLabel').innerText = `${hero.id} - ${hero.name}`;
+      document.querySelector('.modal-body').innerText = '影片';
     };
 
     heroBox.append(heroCard);
